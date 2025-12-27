a1d7760e3ced835261867551c1f8e560a737621c
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-12-27 13:56:51 +0200

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import {useState} from 'react'
+import {Card} from "./components/Card/Card";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>React TypeScript</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <h1>React TypeScript</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+            <Card title="Card title" paragraph="Card paragraph"/>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+type CardProps = {
+    title: string,
+    paragraph: string
+}
+
+export const Card = ({ title, paragraph }: CardProps) => <aside>
+    <h2>{ title }</h2>
+    <p>
+        { paragraph }
+    </p>
+</aside>
